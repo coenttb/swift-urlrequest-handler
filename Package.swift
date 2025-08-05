000ab411779cf154431d38942ef36a01eb3adf7b
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-08-05 06:30:05 +0200

    Fix dependency conflict: use xctest-dynamic-overlay instead of swift-issue-reporting

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ extension Target.Dependency {
     static var dependencies: Self { .product(name: "Dependencies", package: "swift-dependencies") }
     static var dependenciesMacros: Self { .product(name: "DependenciesMacros", package: "swift-dependencies") }
     static var dependenciesTestSupport: Self { .product(name: "DependenciesTestSupport", package: "swift-dependencies") }
-    static var issueReporting: Self { .product(name: "IssueReporting", package: "swift-issue-reporting") }
+    static var issueReporting: Self { .product(name: "IssueReporting", package: "xctest-dynamic-overlay") }
     static var loggingExtras: Self { .product(name: "LoggingExtras", package: "swift-logging-extras") }
 }
 
@@ -32,7 +32,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.9.2"),
-        .package(url: "https://github.com/pointfreeco/swift-issue-reporting", from: "1.0.0"),
+        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.4.3"),
         .package(url: "https://github.com/coenttb/swift-logging-extras", from: "0.0.1")
     ],
     targets: [
