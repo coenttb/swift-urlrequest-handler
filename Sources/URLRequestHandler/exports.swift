f19eb1f969c164e8357152febaca6f943e529a40
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-11-03 15:53:22 +0100

    Clean up: remove test code and debugging output
    - Remove test function from exports.swift
    - Remove debugging output from swift-format workflow
    - Swift-format auto-commit is now confirmed working

--- Sources/URLRequestHandler/exports.swift
+++ Sources/URLRequestHandler/exports.swift
@@ -5,11 +5,3 @@
 #if canImport(FoundationNetworking)
   @_exported import FoundationNetworking
 #endif
-
-// Test function to verify swift-format auto-commit
-func testFormattingTrigger() {
-  let x = 1
-  let y = 2
-  let z = x + y
-  print(z)
-}
