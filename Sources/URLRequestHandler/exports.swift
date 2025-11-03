d2cd62d5dc5ce121525e6258b521d935d4c72fc3
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-11-03 15:06:38 +0100

    test: add poorly formatted code to verify swift-format auto-commit

--- Sources/URLRequestHandler/exports.swift
+++ Sources/URLRequestHandler/exports.swift
@@ -5,3 +5,6 @@
 #if canImport(FoundationNetworking)
 @_exported import FoundationNetworking
 #endif
+
+// Test function to verify swift-format auto-commit
+func testFormattingTrigger(){let x=1;let y=2;let z=x+y;print(z)}
