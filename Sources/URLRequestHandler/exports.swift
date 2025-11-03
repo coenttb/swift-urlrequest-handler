871951b85e51fe9d047c7ab6b5735e56634386e1
coenttb <22656652+coenttb@users.noreply.github.com>
2025-11-03 14:51:57 +0000

    Run swift-format

--- Sources/URLRequestHandler/exports.swift
+++ Sources/URLRequestHandler/exports.swift
@@ -3,8 +3,13 @@
 @_exported import LoggingExtras
 
 #if canImport(FoundationNetworking)
-@_exported import FoundationNetworking
+  @_exported import FoundationNetworking
 #endif
 
 // Test function to verify swift-format auto-commit
-func testFormattingTrigger(){let x=1;let y=2;let z=x+y;print(z)}
+func testFormattingTrigger() {
+  let x = 1
+  let y = 2
+  let z = x + y
+  print(z)
+}
